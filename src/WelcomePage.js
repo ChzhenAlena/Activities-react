56731737241df088833ebcaab4b48c545eb566e8
Adilbek Maratov <bigyellowforjava@gmail.com>
2024-03-19 22:22:26 +0100

    some changes

--- src/WelcomePage.js
+++ src/WelcomePage.js
@@ -19,10 +19,11 @@ function WelcomePage({ username, onLogout }) {
     return (
         <div className="container">
             <h2>You are successfully logged in!</h2>
-            <button className="logout-btn" onClick={logout}>Logout</button>
             <button className="user-page-btn" onClick={handleGoToUserPage}>Go to your activities</button>
             <button className="activities-page-btn" onClick={handleGoToActivitiesPage}>Go to all activities</button>
+            <button className="logout-btn" onClick={logout}>Logout</button>
         </div>
     );
 }
+
 export default WelcomePage;
