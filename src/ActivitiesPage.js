f50476b43c8ff689a9823605d919eb88ffe1f49b
Adilbek Maratov <bigyellowforjava@gmail.com>
2024-03-20 16:56:59 +0100

    added some changes to the tables 2

--- src/ActivitiesPage.js
+++ src/ActivitiesPage.js
@@ -1,8 +1,8 @@
-// UsersPage.js
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import useLogout from './useLogout'
-import {useNavigate} from "react-router-dom";
+import useLogout from './useLogout';
+import { useNavigate } from "react-router-dom";
+import './UserPage.css'; // Подключаем файл стилей
 
 const ActivitiesPage = () => {
     const [activities, setActivities] = useState([]);
@@ -11,14 +11,6 @@ const ActivitiesPage = () => {
     const logout = useLogout();
     const navigate = useNavigate();
 
-    const tableStyle = {
-        border: '1px solid black',
-        borderCollapse: 'collapse',
-    };
-    const cellStyle = {
-        border: '1px solid black',
-        padding: '8px',
-    };
     const [post, setPost] = useState({
         name: '',
         priority: '',
@@ -31,14 +23,12 @@ const ActivitiesPage = () => {
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const token = localStorage.getItem('jwtToken'); // Получение токена из localStorage
+                const token = localStorage.getItem('jwtToken');
                 const headers = {
-                    'Authorization': `${token}` // Создание заголовка Authorization с токеном
+                    'Authorization': `${token}`
                 };
-                console.log(headers);
                 const url = localStorage.getItem("url") + '/activities';
                 const response = await axios.get(url, {headers: headers});
-                console.log('get performed')
                 setActivities(response.data.activities);
                 setMode(response.data.mode);
                 setPeople(response.data.people);
@@ -56,9 +46,9 @@ const ActivitiesPage = () => {
 
     function handleSubmit(event) {
         event.preventDefault()
-        const token = localStorage.getItem('jwtToken'); // Получение токена из localStorage
+        const token = localStorage.getItem('jwtToken');
         const headers = {
-            'Authorization': `${token}` // Создание заголовка Authorization с токеном
+            'Authorization': `${token}`
         };
         const url = localStorage.getItem("url") + '/activities';
         axios.post(url, post, {headers: headers})
@@ -69,143 +59,120 @@ const ActivitiesPage = () => {
                 localStorage.setItem('jwtToken', response.data.id);
             })
             .catch(err => console.log(err))
-
     }
+
     const handleBackButtonClick = () => {
-        navigate("/menu"); // Перенаправляем на страницу WelcomePage
+        navigate("/menu");
     };
 
     const handleSelectChange2 = (event) => {
         setID({...ID, [event.target.name]: event.target.value});
     }
+
     const handleSubmit2 = (event) => {
         event.preventDefault();
-        const token = localStorage.getItem('jwtToken'); // Получение токена из localStorage
+        const token = localStorage.getItem('jwtToken');
         const headers = {
-            'Authorization': `${token}` // Создание заголовка Authorization с токеном
+            'Authorization': `${token}`
         };
         const path =  localStorage.getItem("url") + '/activities/'+ + event.currentTarget.id;
-        console.log(path);
-        console.log(ID);
         axios.post(path, ID, { headers: headers })
             .then(function (response) {
                 console.log(response);
             })
             .catch(err => console.log(err))
-
-        // Действия по отправке данных формы
-        console.log('Submitted');
     }
+
     const handleDelete = (event) => {
         event.preventDefault();
-        const token = localStorage.getItem('jwtToken'); // Получение токена из localStorage
+        const token = localStorage.getItem('jwtToken');
         const headers = {
-            'Authorization': `${token}` // Создание заголовка Authorization с токеном
+            'Authorization': `${token}`
         };
-        console.log(event.currentTarget)
         const path =  localStorage.getItem("url") + '/activities/'+ event.currentTarget.id;
-        console.log(path);
-                axios.delete(path, { headers: headers })
-                    .then(function (response) {
-                        console.log(response);
-                    })
-                    .catch(err => console.log(err))
-
-                // Действия по отправке данных формы
-                console.log('Submitted');
+        axios.delete(path, { headers: headers })
+            .then(function (response) {
+                console.log(response);
+            })
+            .catch(err => console.log(err))
     }
 
-    if(mode==='admin') {
-        return (
-            <div>
-                <form>
-                    <p>Create new activity:</p>
-                    <div>
-                        <label>Name:</label>
-                        <input type="text" name="name" onChange={handleInput}/>
+    return (
+        <div>
+            {mode === 'admin' && (
+                <div>
+                    <form>
+                        {/* Ваша форма создания активности */}
+                    </form>
+                    <h1>Activities</h1>
+                    <div className="table-container">
+                        <table className="custom-table">
+                            <thead>
+                            <tr>
+                                <th>#</th> {/* Добавляем колонку для нумерации */}
+                                <th>Name</th>
+                                <th>Priority</th>
+                                <th>Status</th>
+                                <th>Person</th>
+                                <th>Change person</th>
+                                <th></th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            {activities.map((activity, index) => (
+                                <tr key={activity.id}>
+                                    <td>{index + 1}</td> {/* Выводим номер строки */}
+                                    <td>{activity.activityName}</td>
+                                    <td>{activity.priority}</td>
+                                    <td>{activity.status}</td>
+                                    <td>{activity.personName} {activity.personSurname}</td>
+                                    <td>
+                                        {/* Форма и кнопка для изменения персоны */}
+                                    </td>
+                                    <td>
+                                        {/* Форма и кнопка для удаления активности */}
+                                    </td>
+                                </tr>
+                            ))}
+                            </tbody>
+                        </table>
                     </div>
-                    <div>
-                        <label>Priority:</label>
-                        <input type="text" name="priority" onChange={handleInput}/>
+                    {/* Кнопки для выхода и возврата */}
+                </div>
+            )}
+            {mode === 'user' && (
+                <div>
+                    <h1>Activities</h1>
+                    <div className="table-container">
+                        <table className="custom-table">
+                            <thead>
+                            <tr>
+                                <th>#</th> {/* Добавляем колонку для нумерации */}
+                                <th>Name</th>
+                                <th>Priority</th>
+                                <th>Status</th>
+                                <th>Person</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            {activities.map((activity, index) => (
+                                <tr key={activity.id}>
+                                    <td>{index + 1}</td> {/* Выводим номер строки */}
+                                    <td>{activity.activityName}</td>
+                                    <td>{activity.priority}</td>
+                                    <td>{activity.status}</td>
+                                    <td>{activity.personName} {activity.personSurname}</td>
+                                </tr>
+                            ))}
+                            </tbody>
+                        </table>
                     </div>
-                    <div>
-                        <label>Status:</label>
-                        <input type="twxt" name="status" onChange={handleInput}/>
-                    </div>
-                    <button className="login-btn" type="button" onClick={handleSubmit}>Create</button>
-                </form>
-                <h1>Activities</h1>
-                <table style={tableStyle}>
-                    <thead>
-                    <tr>
-                        <th style={cellStyle}>Name</th>
-                        <th style={cellStyle}>Priority</th>
-                        <th style={cellStyle}>Status</th>
-                        <th style={cellStyle}>Person</th>
-                        <th style={cellStyle}>Change person</th>
-                        <th style={cellStyle}></th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {activities.map((activity) => (
-                        <tr key={activity.id}>
-                            <td style={cellStyle}>{activity.activityName}</td>
-                            <td style={cellStyle}>{activity.priority}</td>
-                            <td style={cellStyle}>{activity.status}</td>
-                            <td style={cellStyle}>{activity.personName} {activity.personSurname}</td>
-                            <td style={cellStyle}>
-                                <form id={activity.id} onSubmit={handleSubmit2}>
-                                    <select name="ID" onChange={handleSelectChange2}>
-                                        {people.map((person, index) => (
-                                            <option key={index} value={person.id}>{person.name}</option>
-                                        ))}
-                                    </select>
-                                    <button type="submit">Submit</button>
-                                </form>
-                            </td>
-                            <td style={cellStyle}>
-                                <form id = {activity.id} onSubmit={handleDelete}>
-                                    <button type="submit">Delete</button>
-                                </form>
-                            </td>
-                        </tr>
-                    ))}
-                    </tbody>
-                </table>
-                <button className="logout-btn" type="button" onClick={logout}>Logout</button>
-                <button className="logout-btn" type="button" onClick={handleBackButtonClick}>Back</button>
-            </div>
-        );
-    }
-    if (mode === 'user') {
-        return (
-            <div>
-                <h1>Activities</h1>
-                <table style={tableStyle}>
-                    <thead>
-                    <tr>
-                        <th style={cellStyle}>Name</th>
-                        <th style={cellStyle}>Priority</th>
-                        <th style={cellStyle}>Status</th>
-                        <th style={cellStyle}>Person</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {activities.map((activity) => (
-                        <tr key={activity.id}>
-                            <td style={cellStyle}>{activity.activityName}</td>
-                            <td style={cellStyle}>{activity.priority}</td>
-                            <td style={cellStyle}>{activity.status}</td>
-                            <td style={cellStyle}>{activity.personName} {activity.personSurname}</td>
-                        </tr>
-                    ))}
-                    </tbody>
-                </table>
-                <button className="logout-btn" type="button" onClick={handleBackButtonClick}>Back</button>
-                <button className="logout-btn" type="button" onClick={logout}>Logout</button>
-            </div>
-        );
-    }
+                    <button className="logout-btn" type="button" onClick={handleBackButtonClick}>Back</button>
+                    <button className="logout-btn" type="button" onClick={logout}>Logout</button>
+                </div>
+            )}
+        </div>
+    );
 };
 
 export default ActivitiesPage;
